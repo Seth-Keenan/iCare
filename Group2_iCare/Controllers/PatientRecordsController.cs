b212c8638aa75965c2ef1b67f9dd94fd1fa99233
Seth Keenan <seth.j.keenan@gmail.com>
2024-10-25 09:09:17 -0500

    Added some views and controllers
    Add controllers and views for patient and treatment records
    
    Added `PatientRecordsController` and `TreatmentRecordsController` to manage patient and treatment records, respectively. Updated `Group2_iCare.csproj` to include the new controllers and their views. Added views for creating, deleting, viewing details, editing, and listing patient and treatment records. The `TreatmentRecords` index view is strongly typed to `IEnumerable<Group2_iCare.Models.TreatmentRecord>` and includes a table displaying `Description` and `TreatmentDate` with links to edit, view, and delete each record.

---
+++ Group2_iCare/Controllers/PatientRecordsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Group2_iCare.Models;
+
+namespace Group2_iCare.Controllers
+{
+    public class PatientRecordsController : Controller
+    {
+        private Group2_ICAREDBEntities db = new Group2_ICAREDBEntities();
+
+        // GET: PatientRecords
+        public ActionResult Index()
+        {
+            return View(db.PatientRecords.ToList());
+        }
+
+        // GET: PatientRecords/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PatientRecord patientRecord = db.PatientRecords.Find(id);
+            if (patientRecord == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patientRecord);
+        }
+
+        // GET: PatientRecords/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PatientRecords/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Address,DateOfBirth,Height,Weight,BloodGroup,BedID,TreatmentArea")] PatientRecord patientRecord)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PatientRecords.Add(patientRecord);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(patientRecord);
+        }
+
+        // GET: PatientRecords/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PatientRecord patientRecord = db.PatientRecords.Find(id);
+            if (patientRecord == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patientRecord);
+        }
+
+        // POST: PatientRecords/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Address,DateOfBirth,Height,Weight,BloodGroup,BedID,TreatmentArea")] PatientRecord patientRecord)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(patientRecord).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(patientRecord);
+        }
+
+        // GET: PatientRecords/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PatientRecord patientRecord = db.PatientRecords.Find(id);
+            if (patientRecord == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patientRecord);
+        }
+
+        // POST: PatientRecords/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            PatientRecord patientRecord = db.PatientRecords.Find(id);
+            db.PatientRecords.Remove(patientRecord);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
