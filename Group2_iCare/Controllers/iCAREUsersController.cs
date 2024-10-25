0202581581199fa6b860c66564a1df7e12fcdf97
Seth Keenan <seth.j.keenan@gmail.com>
2024-10-25 09:02:11 -0500

    Finished following Ouda 19 Steps
    Add iCAREUser CRUD operations and update EntityFramework
    
    - Added `iCAREUsersController` for managing `iCAREUser` entities.
    - Created views for `iCAREUser` CRUD operations: `Create.cshtml`, `Delete.cshtml`, `Details.cshtml`, `Edit.cshtml`, `Index.cshtml`.
    - Updated `Group2_iCare.csproj` to include the new controller and views.
    - Modified home page `Index.cshtml` to include CRS info and navigation links.
    - Updated `Group2_iCare.csproj` and `packages.config` to use EntityFramework 6.4.4.

---
+++ Group2_iCare/Controllers/iCAREUsersController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Group2_iCare.Models;
+
+namespace Group2_iCare.Controllers
+{
+    public class iCAREUsersController : Controller
+    {
+        private Group2_ICAREDBEntities db = new Group2_ICAREDBEntities();
+
+        // GET: iCAREUsers
+        public ActionResult Index()
+        {
+            var iCAREUsers = db.iCAREUsers.Include(i => i.UserPassword);
+            return View(iCAREUsers.ToList());
+        }
+
+        // GET: iCAREUsers/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            iCAREUser iCAREUser = db.iCAREUsers.Find(id);
+            if (iCAREUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(iCAREUser);
+        }
+
+        // GET: iCAREUsers/Create
+        public ActionResult Create()
+        {
+            ViewBag.ID = new SelectList(db.UserPasswords, "ID", "UserName");
+            return View();
+        }
+
+        // POST: iCAREUsers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name")] iCAREUser iCAREUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.iCAREUsers.Add(iCAREUser);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ID = new SelectList(db.UserPasswords, "ID", "UserName", iCAREUser.ID);
+            return View(iCAREUser);
+        }
+
+        // GET: iCAREUsers/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            iCAREUser iCAREUser = db.iCAREUsers.Find(id);
+            if (iCAREUser == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ID = new SelectList(db.UserPasswords, "ID", "UserName", iCAREUser.ID);
+            return View(iCAREUser);
+        }
+
+        // POST: iCAREUsers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name")] iCAREUser iCAREUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(iCAREUser).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ID = new SelectList(db.UserPasswords, "ID", "UserName", iCAREUser.ID);
+            return View(iCAREUser);
+        }
+
+        // GET: iCAREUsers/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            iCAREUser iCAREUser = db.iCAREUsers.Find(id);
+            if (iCAREUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(iCAREUser);
+        }
+
+        // POST: iCAREUsers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            iCAREUser iCAREUser = db.iCAREUsers.Find(id);
+            db.iCAREUsers.Remove(iCAREUser);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
