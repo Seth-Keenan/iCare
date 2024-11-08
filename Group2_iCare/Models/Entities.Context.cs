86d8255b8ad3be70284625e6869b6dcc23fb3c8d
Tin-Duong1 <114547634+Tin-Duong1@users.noreply.github.com>
2024-11-07 20:08:53 -0600

    update

--- Group2_iCare/Models/Entities.Context.cs
+++ Group2_iCare/Models/Entities.Context.cs
@@ -37,5 +37,6 @@ namespace Group2_iCare.Models
         public virtual DbSet<TreatmentRecord> TreatmentRecord { get; set; }
         public virtual DbSet<UserPassword> UserPassword { get; set; }
         public virtual DbSet<UserRole> UserRole { get; set; }
+        public virtual DbSet<File> Files1 { get; set; }
     }
 }
