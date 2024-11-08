694cb871809a8306f0c789171712284377a40531
Seth Keenan <seth.j.keenan@gmail.com>
2024-11-08 14:57:23 -0600

    Display palette in format rakib wanted

--- Group2_iCare/Controllers/DisplayMyBoardController.cs
+++ Group2_iCare/Controllers/DisplayMyBoardController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Web.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 namespace Group2_iCare.Controllers
 {
--- Group2_iCare/Controllers/DisplayPaletteController.cs
+++ Group2_iCare/Controllers/DisplayPaletteController.cs
@@ -15,9 +15,7 @@ namespace Group2_iCare.Controllers
         // GET: DisplayPalette
         public ActionResult Index()
         {
-            var files = db.Files.AsEnumerable();
-            var documentMetadata = db.DocumentMetadata.Include(d => d.iCAREUser).Include(d => d.PatientRecord).Include(d => d.iCAREWorker).Include(d => d.ModificationHistory).AsEnumerable();
-            return View((documentMetadata, files));
+            return View();
         }
 
         // GET: DisplayPalette/Details/5
